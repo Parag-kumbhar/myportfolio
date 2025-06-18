f4cc2fa019662caca1510d5e108e2e493526d6be
Parag-kumbhar <paragkumbhar02@gmail.com>
2025-06-18 11:11:38 +0530

    Initial commit

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,14 @@
+import './Footer.css'
+
+const Footer = () => (
+  <footer className='footer'>
+    <a
+      href='https://github.com/Parag-kumbhar'
+      className='link footer__link'
+    >
+      Created By Parag Kumbhar
+    </a>
+  </footer>
+)
+
+export default Footer
---
+++ src/portfolio.js
@@ -0,0 +1,77 @@
+const header = {
+  // all the properties are optional - can be left empty or deleted
+  homepage: 'https://linkedin.com/in/parag-kumbhar',
+  title: 'PRK.',
+}
+
+const about = {
+  // all the properties are optional - can be left empty or deleted
+  name: 'Parag Kumbhar',
+  role: 'Full Stack Engineer',
+  description:
+  'Outgoing and personable professional as well as an explorer. Teamwork-oriented, growth-focused, and performance-driven. Pursuing B.Tech in Computer Science Engineering with a passion for scalable web applications.',
+  resume: 'https://drive.google.com/file/d/1EL9_7q9zbfx2qJlalDEZqfayK64E-3q8/view?usp=drive_link',
+  social: {
+    linkedin: 'https://linkedin.com/in/parag-kumbhar',
+    github: 'https://github.com/Parag-kumbhar',
+  },
+}
+
+const projects = [
+  // projects can be added an removed
+  // if there are no projects, Projects section won't show up
+  {
+    name: 'Ishwarya-BI',
+    description:
+      'It is basically normal project make the Internship company Ishwarya-BI website developed using React.js , CSS, HTML, and JavaScript.',
+    stack: ['React.js' , 'CSS', 'HTML',  'JavaScript'],
+    sourceCode: 'https://github.com/Parag-kumbhar/Ishwarya-BI',
+    livePreview: 'https://parag-kumbhar.github.io/Ishwarya-BI/',
+  },
+  {
+    name: 'Voters Directory Project',
+    description:
+    'Built using React.js, Node.js, Express.js, and PostgreSQL. Features include data fetching, updating, file uploads with Multer, and secure password hashing with bcrypt.',
+    stack: ['React.js', 'Node.js', 'Express.js', 'PostgreSQL'],
+    sourceCode: 'https://github.com/Parag-kumbhar/voters-directory', // Update with your project repo
+    livePreview: 'https://github.com',
+  },
+  {
+    name: 'GYM Management System',
+    description:
+    'Developed a user-friendly web interface using HTML, CSS, PHP, and CodeIgniter 4 framework, applying MVC pattern for enhanced performance.',
+    stack: ['HTML', 'CSS', 'PHP', 'CodeIgniter 4'],
+    sourceCode: 'https://github.com',
+    livePreview: 'https://github.com',
+  },
+  
+]
+
+const skills = [
+  'JavaScript ',
+  'HTML5',
+  'CSS3',
+  'Next.js',
+  'Express.js',
+  'PHP',
+  
+  'CodeIgniter',
+  // 'Python',
+  'Java',
+  'TypeScript',
+  'React.js',
+  'Node.js',
+  'Bootstrap',
+  'Tailwind CSS',
+  'RESTful APIs',
+  'Postman',
+  // 'MongoDB',
+  'PostgreSQL',
+]
+
+const contact = {
+  // email is optional - if left empty Contact section won't show up
+  email: 'paragkumbhar02@gmail.com',
+}
+
+export { header, about, projects, skills, contact }
