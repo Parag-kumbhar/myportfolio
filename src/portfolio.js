52b764560f54c91d03ff4dc2dfdb651ac1106f9d
Parag-kumbhar <paragkumbhar02@gmail.com>
2025-10-13 12:43:58 +0530

    Updated portfolio with Resume changes

--- src/portfolio.js
+++ src/portfolio.js
@@ -10,7 +10,7 @@ const about = {
   role: 'Full Stack Engineer',
   description:
   'Outgoing and personable professional as well as an explorer. Teamwork-oriented, growth-focused, and performance-driven. Pursuing B.Tech in Computer Science Engineering with a passion for scalable web applications.',
-  resume: 'https://drive.google.com/file/d/1EL9_7q9zbfx2qJlalDEZqfayK64E-3q8/view?usp=drive_link',
+  resume: 'https://drive.google.com/file/d/1JZ1AfkQkCGV02zW_r7oA_UjQsAVKz-d8/view?usp=sharing',
   social: {
     linkedin: 'https://linkedin.com/in/parag-kumbhar',
     github: 'https://github.com/Parag-kumbhar',
